885f134834bdd727284ea8c72b196999b6a5f49e
Sherman Chen <schen@simviu.com>
2023-10-30 21:17:25 +0800

    [oled]: build ok esp32.

--- src/esp32/oled_esp.cpp
+++ src/esp32/oled_esp.cpp
@@ -21,15 +21,15 @@ bool OLED::write(int vpos,
     const string& s, int font_sz)
 {
     if(font_sz <3)
-        ssd1306_display_text(&dev_, vpos, s.c_str(), s.length(), false);
+        ssd1306_display_text(&dev_, vpos, (char*)s.c_str(), s.length(), false);
     else
-    	ssd1306_display_text_x3(&dev_, vpos, s.c_str(), s.length(), false);
+    	ssd1306_display_text_x3(&dev_, vpos, (char*)s.c_str(), s.length(), false);
 
     return true;
 }
 //---
 void OLED::clear()
 {
-    ssd1306_clear_screen(&dev, false);
-	ssd1306_contrast(&dev, 0xff);
+    ssd1306_clear_screen(&dev_, false);
+	ssd1306_contrast(&dev_, 0xff);
 }
